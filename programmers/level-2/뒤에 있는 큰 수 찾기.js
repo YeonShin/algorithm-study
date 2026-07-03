6ed0b1422f353815f21b3eae0bb3b1c3be1d6220
YeonShin <yeonnim82@gmail.com>
2026-07-03 16:12:48 +0900

    solve: 뒤에 있는 큰 수 찾기 (Lv. 2)

---
+++ programmers/level-2/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,16 @@
+// Stack
+function solution(numbers) {
+    var answer = new Array(numbers.length).fill(-1);
+    
+    let stack = []
+    
+    for (let i=0; i<numbers.length; i++) {
+        while (stack.length > 0 && numbers[stack[stack.length - 1]] < numbers[i]) {
+            const targetIdx = stack.pop();
+            answer[targetIdx] = numbers[i]
+        }
+        stack.push(i)
+    }
+    
+    return answer;
+}

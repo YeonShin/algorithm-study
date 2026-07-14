7ac12c822404e1c60df401ed6d1aca8b48140195
YeonShin <yeonnim82@gmail.com>
2026-07-14 17:56:15 +0900

    누락된 비밀 코드 해독 코드 추가

--- programmers/level-2/비밀 코드 해독.js
+++ programmers/level-2/비밀 코드 해독.js
@@ -1,1 +1,49 @@
-// DFS 
+// DFS & Brute Force
+function solution(n, q, ans) {
+    var answer = 0;
+    
+    const currentCombination = [];
+    
+    function makeCombination(start, depth) {
+        
+        if (depth === 5) {
+            if (checkValid(currentCombination, q, ans)) {
+                answer++;   
+            }
+            return;
+        }
+        
+        
+        
+        for (let i=start; i <= n; i++) {
+            currentCombination.push(i);
+            makeCombination(i + 1, depth + 1);
+            currentCombination.pop();
+        }
+    }
+    
+    makeCombination(1, 0);
+    
+    
+    
+    return answer;
+}
+
+function checkValid(current, q, ans) {
+    for (let i=0; i<q.length; i++) {
+        const currentQ = q[i];
+        const expectedAns = ans[i];
+        
+        let matchCount = 0;
+        for (const num of current) {
+            if (currentQ.includes(num)) {
+                matchCount++;
+            }
+        }
+        
+        if (matchCount !== expectedAns) {
+            return false
+        }
+    }
+    return true
+}
